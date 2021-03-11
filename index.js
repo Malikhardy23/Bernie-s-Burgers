fef818fdf48326a012e16c2400461c3958338008
Malik Hardy <50418576+Malikhardy23@users.noreply.github.com>
2021-03-11 17:30:35 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,18 +8,20 @@ let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
-const newEventModal = document.getElementById("newEventModal");
-const backDrop = document.getElementById("modalBackDrop")
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput')
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-
-const openModal = (date) => {
+function openModal(date){
     clicked = date;
 
     const eventForDay = events.find(e => e.date === clicked);
 
     if(eventForDay){
-        console.log('Event already exists');
+        document.getElementById('eventText').innerText = eventForDay.title;
+        deleteEventModal.style.display = 'block';
     } else {
         newEventModal.style.display = 'block';
     }
@@ -49,7 +51,7 @@ const load = () => {
         weekday: 'long',
         year: 'numeric',
         month: 'numeric',
-        day: 'numeric'
+        day: 'numeric',
     });
 
     const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
@@ -62,9 +64,25 @@ const load = () => {
         const daySquare = document.createElement('div');
         daySquare.classList.add('day');
 
+        const dayString = `${month + 1}/${i - paddingDays}/${year}`
+
         if(i > paddingDays){
             daySquare.innerText = i - paddingDays;
-            daySquare.addEventListener('click', () => openModal(`${month + 1}/${i - paddingDays}/${year}`));
+
+            const eventForDay = events.find(e => e.date === dayString);
+
+            if(i - paddingDays === day && nav === 0){
+                daySquare.id = 'currentDay';
+            }
+
+            if(eventForDay){
+                const eventDiv = document.createElement('div');
+                eventDiv.classList.add('event');
+                eventDiv.innerText = eventForDay.title;
+                daySquare.appendChild(eventDiv);
+            }
+
+            daySquare.addEventListener('click', () => openModal(dayString));
         } else {
           daySquare.classList.add('padding');
         }
@@ -72,6 +90,39 @@ const load = () => {
         calendar.appendChild(daySquare)
     }
 
+}
+
+const closeModal = () => {
+    eventTitleInput.classList.remove('error')
+    newEventModal.style.display = 'none';
+    deleteEventModal.style.display = 'none';
+    backDrop.style.display = 'none';
+    eventTitleInput.value = '';
+    clicked = null;
+    load();
+
+}
+
+const saveEvent = () => {
+    if(eventTitleInput.value){
+        eventTitleInput.classList.remove('error');
+
+        events.push({
+            date: clicked,
+            title: eventTitleInput.value,
+        });
+
+        localStorage.setItem('events', JSON.stringify(events));
+        closeModal();
+    } else {
+        eventTitleInput.classList.add('error');
+    }
+}
+
+const deleteEvent = () => {
+    events = events.filter(e => e.date !== clicked);
+    localStorage.setItem('events', JSON.stringify(events));
+    closeModal();
 }
 
 const initButtons = () => {
@@ -84,6 +135,17 @@ const initButtons = () => {
         nav--;
         load();
     })
+
+   document.getElementById('saveButton').addEventListener('click', saveEvent)
+
+    document.getElementById('cancelButton').addEventListener('click', closeModal);
+
+
+   document.getElementById('deleteButton').addEventListener('click', deleteEvent)
+
+   document.getElementById('closeButton').addEventListener('click', closeModal);
+
+
 }
 
 initButtons();
